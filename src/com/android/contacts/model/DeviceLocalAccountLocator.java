6bfc69ec1fc96a26e067890996a87c2ee216ce94
Toha <tohenk@yahoo.com>
2018-02-13 12:58:31 +0700

    Contacts: Enable support for device contact.
    Change-Id: I8f2e907deadced5316be5a418dab2538476a4033
    Signed-off-by: Toha <tohenk@yahoo.com>
    
    Contacts: Ignore device null account.
    
    As of commit 2cb5e0752a8def94c386a02213c54bb1be4b6344 which adds device contact,
    the behavior is correct if there is a google account already setup, otherwise
    there are two device contacts shown. So, always ignore null account.
    
    Change-Id: I9f18a45efcaa93b8583013beb5995bf85ae58dcd
    Signed-off-by: Toha <tohenk@yahoo.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/contacts/model/DeviceLocalAccountLocator.java
+++ src/com/android/contacts/model/DeviceLocalAccountLocator.java
@@ -23,7 +23,6 @@ import android.provider.ContactsContract;
 
 import com.android.contacts.Experiments;
 import com.android.contacts.model.account.AccountWithDataSet;
-import com.android.contacts.model.account.GoogleAccountType;
 import com.android.contactsbind.ObjectFactory;
 import com.android.contactsbind.experiments.Flags;
 
@@ -71,7 +70,7 @@ public abstract class DeviceLocalAccountLocator {
             return new Cp2DeviceLocalAccountLocator(context.getContentResolver(),
                     ObjectFactory.getDeviceLocalAccountTypeFactory(context), knownTypes);
         } else {
-            return new NexusDeviceAccountLocator(context, accountManager);
+            return new NexusDeviceAccountLocator();
         }
     }
 
@@ -84,26 +83,9 @@ public abstract class DeviceLocalAccountLocator {
      * </p>
      */
     public static class NexusDeviceAccountLocator extends DeviceLocalAccountLocator {
-        private final Context mContext;
-        private final AccountManager mAccountManager;
-
-
-        public NexusDeviceAccountLocator(Context context, AccountManager accountManager) {
-            mContext = context;
-            mAccountManager = accountManager;
-        }
-
         @Override
         public List<AccountWithDataSet> getDeviceLocalAccounts() {
-            @SuppressWarnings("MissingPermission")
-            final Account[] accounts = mAccountManager
-                    .getAccountsByType(GoogleAccountType.ACCOUNT_TYPE);
-
-            if (accounts.length > 0 && !AccountWithDataSet.getNullAccount().hasData(mContext)) {
-                return Collections.emptyList();
-            } else {
-                return Collections.singletonList(AccountWithDataSet.getNullAccount());
-            }
+            return Collections.emptyList();
         }
     }
 }
